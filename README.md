95e8af714a42171fe37302e5b3006dbe543c352e
Chase.Thomas <Chase.Thomas@Workday.com>
2021-08-11 18:32:35 -0700

    finished class model + added age in years and age in days

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ How many days have you been alive? How many more do you have left? Let's find ou
 
  - [x] Figure out how to calculate the numbers of days lived. Add 
  - [x] Create repo and push first code
- - [ ] Convert code using class model 
- - [ ] Make it easy to show new calculations as added
- - [ ] totalYears calculation
+ - [x] Convert code using class model 
+ - [x] Make it easy to show new calculations as added
+ - [x] totalYears calculation
+ - [x] totalDays calculation
  - [ ] totalYearsMonthsDays calculation
- - [ ] totalDays calculation
  - [ ] totalTime calculation
  - [ ] totalYearWeeksDaysHours calculation
  - [ ] Create lifeExpectancy method
@@ -21,6 +21,15 @@ How many days have you been alive? How many more do you have left? Let's find ou
 
 ### Journal
   
+*August 11th, 2021*
+Refactored a lot of the code and finally got it working with the class and function model I immagined. It's far from perfect, but a good starting point. 
+
+One thing I realized is that I really don't know what I'm doing when it comes to object oriented. All I knew is that I wanted to use a class structure, but I didn't know if I should use setters and getters, or what should be in the person class or not. What I settled on was putting anything specific to the user inside the class, and everything generic outside. 
+
+I added a function to add new html to the UI. This allows me to append any number of things to the page, such as age in years, and in days, etc. 
+
+I'm not sure if my age in years calculation will hold up. Might be worth revisiting. 
+
 *August 10th, 2021*
 
 Created a "person" class to gather and hold user information. Right now will only log user's information to console. 
--- index.js
+++ index.js
@@ -1,69 +1,98 @@
 class Person {
-    constructor(name, dob){
-        this.name = name;
-        this.dob = dob;
-    }
-    calculateDaysLived(inputDate){
-        console.log("Input Date: " + inputDate);
-
-        // Get current date
-        const d = new Date();
-        let currentYear = d.getFullYear();
-        
-        // Number of days this year
-        let today = new Date();
-        let daysThisYear = Math.floor(
-        (today - new Date(today.getFullYear(), 0, 1)) / 86400000);
-
-        // Calculate date of birth
-        let dateTime = inputDate + "T00:00:00";
-        let dob = new Date(dateTime);
-
-        // Number of days from birthday to end of year
-        let daysInBirthYear = 109;
-
-        // Get year
-        let birthYear = dob.getFullYear();
-        
-        // Leap year calculator
-        let totalLeaps = 0, totalNorms = 0;
-        let calculateYear = function (year) {
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-        }
-        // Add up leap years and regular years
-        let ageInDays = 0;
-        for (var i = birthYear+1; i < currentYear; i++) {
-            if(calculateYear(i)){
-                totalLeaps += 1;
-                ageInDays += 366;
-            } else {
-                totalNorms += 1;
-                ageInDays += 365;
-            }
-        }
-        // Add the days
-        ageInDays = daysInBirthYear + ageInDays + daysThisYear;
-        return ageInDays;
-    }
-    calculateTimeLeft(){
-        return "Time left: unknown"
-    }
-    getTimeZone(){
-        const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        console.log(tz);
-    }
+  constructor(name, dob) {
+    this.name = name;
+    this.dob = new Date(dob);
+  }
+  // Return year of birth
+  birthYear() {
+    return this.dob.getFullYear();
+  }
+  // return month of birth as number
+  birthMonth() {
+    // +1 becuase getMonth is zero indexed
+    return this.dob.getMonth() + 1;
+  }
+  // Return day of month in which user born
+  birthDate() {
+    return this.dob.getDate();
+  }
+  // Return the number of days in year after user was born
+  daysAfterBirth() {  
+    // How many days in the year of birth?
+    let totalDays = calculateDaysInAYear(this.birthMonth());
+    // On which day of the year were they born?
+    let dayOfBirth = calculateDayOfyear(this.dob);
+    // Subtract the day of birth from total days that year
+    return totalDays - dayOfBirth;
+  }
+  // Return the user's age in years
+  ageInYears() {
+    let currentYear = new Date().getFullYear();
+    return currentYear - this.birthYear() - 1;
+  }
+  // Return the user's age in days
+  ageInDays() {
+    // Number of days this year
+    let today = new Date();
+    // Number of days from birthday to end of birth year
+    let daysInBirthYear = this.daysAfterBirth();
+    // Total days between birth and current year
+    let ageInDays = calculateTotalDaysBetweenYears(this.birthYear(), today.getFullYear());
+    // Number of days this year
+    let daysThisYear = calculateDayOfyear(today);
+    // Add the days
+    ageInDays += daysInBirthYear + daysThisYear;
+    return ageInDays;
+  }
 }
 
+// Determine how many days are in the current month
+let daysInMonth = function(year, month){
+    return new Date(year, month, 0).getDate();
+}
 
-document.getElementById("submit").addEventListener("click", function(){
+// Return the day number of the year
+let calculateDayOfyear = function (date) {
+    let total = 0;
+    for (var i = 1; i < date.getMonth() + 1; i++) {
+        total += daysInMonth(date.getFullYear(), i);
+    }
+    total += date.getDate();
+    return total;
+};
+
+// Takes in a year and returns 366 if leap year, 365 if not
+let calculateDaysInAYear = function (year) {
+    // If leap year return 366, else 365
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        return 366;
+    } else {
+        return 365;
+    }
+};
+
+// Counts the number of days between two given years
+let calculateTotalDaysBetweenYears = function (startYear, currentYear) {
+    // Add up leap years and regular years
+    let totalDays = 0;
+    for (var i = startYear + 1; i < currentYear; i++) {
+        totalDays += calculateDaysInAYear(i);
+    }
+    return totalDays;
+};
+
+// Adds a new HTML element to the UI
+let addElement = function(code) {
+    document.getElementById("lifeFacts").innerHTML += "</br>" + code + "</br>";
+}
+
+// Run code when user clicks submit button
+document.getElementById("submit").addEventListener("click", function () {
     let userName = document.getElementById("name").value;
     let inputDate = document.getElementById("dob").value;
-    let dayNumber = document.getElementById("dayNumber");
+    let user = new Person(userName, inputDate);
 
-    let user = new Person(userName, inputDate)
-
-    console.log(user.name + "\n" + inputDate)
-
-    // Calculate days of life from input date
-    dayNumber.innerText = user.calculateDaysLived(inputDate);
-})
+    // Append results to UI
+    addElement("Years old: " + user.ageInYears());
+    addElement("Days old: " + user.ageInDays());
+});
